7c778d10ad7b87819acf5591657203a5f7874469
alexs <alexstlopes@gmail.com>
2022-06-05 21:23:18 -0300

    Configurado Resource Srever

--- src/main/java/com/cabidiomas/cadastro/config/ResourceServerConfig.java
+++ src/main/java/com/cabidiomas/cadastro/config/ResourceServerConfig.java
@@ -1,4 +1,19 @@
 package com.cabidiomas.cadastro.config;
 
-public class ResourceServerConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .antMatchers("/api/usuarios").permitAll()
+                .antMatchers("/api/aluno/**", "/api/cursos/**").authenticated()
+                .anyRequest().denyAll();
+    }
 }
